63baa75e5fe4ad065da8cdc9cf270bc36d4bf974
Mihaela Catan <mihaelacatan7@gmail.com>
2025-05-21 11:39:02 +0300

    Split into files

--- DSL_I/src/main/java/src/GravITyIDE.java
+++ DSL_I/src/main/java/src/GravITyIDE.java
@@ -4387,39 +4387,3 @@ public class GravITyIDE extends JFrame {
     }
 }
 
-class TextAreaOutputStream extends java.io.OutputStream {
-    private JTextArea textArea;
-
-    public TextAreaOutputStream(JTextArea textArea) {
-        this.textArea = textArea;
-    }
-
-    @Override
-    public void write(int b) throws java.io.IOException {
-        textArea.append(String.valueOf((char) b));
-        textArea.setCaretPosition(textArea.getDocument().getLength());
-    }
-
-
-    public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    GravITyIDE ide = new GravITyIDE();
-                    ide.setVisible(true);
-                    System.out.println("IDE started successfully");
-                } catch (Exception e) {
-                    System.err.println("Error starting IDE: " + e.getMessage());
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-}
